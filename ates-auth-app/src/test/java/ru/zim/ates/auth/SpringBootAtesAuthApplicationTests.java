0bc017fdd11978e0a40ba175264476522e9b3405
zaharovim@multicarta.ru <zaharovim@multicarta.ru>
2021-11-07 22:35:01 +0300

    init commit

---
+++ ates-auth-app/src/test/java/ru/zim/ates/auth/SpringBootAtesAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zim.ates.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringBootAtesAuthApplication.class)
+class SpringBootAtesAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ates-task-app/src/test/java/ru/zim/ates/tasktracker/SpringBootAtesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zim.ates.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringBootAtesTaskTrackerApplication.class)
+class SpringBootAtesTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
